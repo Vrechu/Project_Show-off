2359247812d05152e570db2a9d2df50210bc817c
Freek Michielsen <freek.mich@gmail.com>
2022-04-27 10:35:11 +0200

    Add character jump

---
+++ Project_Show-off/Assets/Scripts/JumpCharacter.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpCharacter : MonoBehaviour
+{
+    private Rigidbody objectRigidbody;
+
+    [SerializeField] private float jumpSpeed = 10000;
+
+    private float jumpInput;
+
+    void Start()
+    {
+        objectRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        SetImput();
+        Jump();
+    }
+
+    /// <summary>
+    /// Assigns the input axis value to the jump value
+    /// </summary>
+    private void SetImput()
+    {
+        jumpInput = Input.GetAxis("Jump");
+    }
+
+    private void Jump()
+    {
+        if (GetComponent<CheckIfGrounded>().IsGrounded())
+        {
+            objectRigidbody.AddForce(transform.up * jumpInput * jumpSpeed * Time.deltaTime);
+        }
+    }
+}
