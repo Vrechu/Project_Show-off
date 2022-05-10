3831fe853126ad5eb1cb97ab19cbb45ffeef3a50
Freek Michielsen <freek.mich@gmail.com>
2022-05-10 14:12:24 +0200

    Add falling tile

---
+++ Project_Show-off/Assets/Scripts/Environment/FallingTile.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingTile : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    [SerializeField] private float fallDownTime = 10;
+    [SerializeField] private float fallDownTimer = 0;
+    private bool hasFallen = false;
+
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        fallDownTimer = fallDownTime;
+    }
+
+    void Update()
+    {
+        FallDownCountdown();
+    }
+
+    private void FallDownCountdown()
+    {
+        if (!hasFallen)
+        {
+            if (fallDownTimer > 0)
+            {
+                fallDownTimer -= Time.deltaTime;
+            }
+            else
+            {
+                rigidbody.isKinematic = false;
+                hasFallen = true;
+            }
+        }
+    }
+}
