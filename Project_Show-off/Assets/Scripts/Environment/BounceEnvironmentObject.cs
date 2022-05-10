5316f2f88630d6a793e35c5e4a1d9dcd8b85979e
Freek Michielsen <freek.mich@gmail.com>
2022-05-10 14:52:01 +0200

    Add delayde bounce

--- Project_Show-off/Assets/Scripts/Environment/BounceEnvironmentObject.cs
+++ Project_Show-off/Assets/Scripts/Environment/BounceEnvironmentObject.cs
@@ -5,13 +5,61 @@ using UnityEngine;
 public class BounceEnvironmentObject : MonoBehaviour
 {
     [SerializeField] private float bounceForce = 500;
+    [SerializeField] private bool isDelayed = false;
+    [SerializeField] private float delayTime = 1;
+    [SerializeField] private float delayTimer = 0;
+    [SerializeField] private float resetTime = 3;
+    [SerializeField] private float resetTimer = 0;
+
+    private void Start()
+    {
+        delayTimer = delayTime;
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
+        if (collision.gameObject.tag == "Player"
+            && !isDelayed)
+        {
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(transform.up * bounceForce);            
+        }
+    }
+
+    private void Update()
+    {
+        if (isDelayed)
+        {
+            ResetCounter();
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
         if (collision.gameObject.tag == "Player")
         {
-            collision.gameObject.GetComponent<Rigidbody>().AddForce(transform.up * bounceForce);
-            //collision.gameObject.GetComponent<Rigidbody>().AddForce(collision.relativeVelocity * - 2, ForceMode.VelocityChange);
+            DelayCounter();
+            if (delayTimer < 0)
+            {
+                collision.gameObject.GetComponent<Rigidbody>().AddForce(transform.up * bounceForce);
+                delayTimer = delayTime;
+                resetTimer = resetTime;
+            }
+        } 
+    }
+
+    private void DelayCounter()
+    {
+        if (delayTimer >= 0 && resetTimer < 0)
+        {
+            delayTimer -= Time.deltaTime;
+        }
+    }
+
+    private void ResetCounter()
+    {
+        if (resetTimer >= 0)
+        {
+            resetTimer -= Time.deltaTime;
         }
     }
 }
